ba0de4cb7a663dfc84cde6b984191fe2de3f9852
Paulovcarraro <paulovcarraro@gmail.com>
2025-05-23 20:18:47 -0300

    Adição de script para exibição do nome do curso na página de formulário e ajustes no código

--- JavaScript/formulario.js
+++ JavaScript/formulario.js
@@ -1,1 +1,9 @@
- 
+//  Script para exibição do nome do curso
+
+const params = new URLSearchParams(window.location.search);
+const curso = params.get("curso") || "Curso não especificado";
+
+document.getElementById("curso-selecionado").style.color = "black";
+document.getElementById("nome-curso").textContent = curso;
+document.getElementById("nome-curso").style.color = "#4CAF50";
+document.getElementById("nome-curso").style.fontWeight = "700";
